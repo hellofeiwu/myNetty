82e85fa9f86a743361780b42c5e1a7e7e154b759
hellofeiwu <hellofeiwu@gmail.com>
2025-08-26 15:11:04 +0800

    使用不确定参数个数形式传递参数

--- client/src/main/java/com/imooc/netty/Refelction.java
+++ client/src/main/java/com/imooc/netty/Refelction.java
@@ -1,5 +1,6 @@
 package com.imooc.netty;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Refelction {
@@ -9,7 +10,7 @@ public class Refelction {
         Object p = clazz.getConstructor().newInstance();
         Method setName = clazz.getMethod("setName", String.class);
         setName.invoke(p,"Tom");
-        Method sayHello = clazz.getMethod("sayHello");
+        Method sayHello = clazz.getMethod("sayHello", int.class, String.class,String.class);
 
         Method[] methods = clazz.getMethods();
         for(Method m : methods){
@@ -18,12 +19,16 @@ public class Refelction {
                 int times= annotation.times();
                 System.out.println("found Runtimes, will run "+ times +" times");
                 for (int i=0;i<times;i++) {
-                    m.invoke(p);
+                    myInvoke(m,p);
                 }
             }
         }
 
-        sayHello.invoke(p);
+        myInvoke(sayHello,p, 10," aaa ", " bbb ");
+    }
+
+    public static Object myInvoke(Method m, Object t, Object... params) throws InvocationTargetException, IllegalAccessException {
+        return m.invoke(t, params);
     }
 }
 
@@ -32,8 +37,8 @@ class Person {
     private String name;
     private int age;
 
-    public void sayHello(){
-        System.out.println("Hello I am " + name);
+    public void sayHello(int msg1, String msg2, String msg3){
+        System.out.println("Hello I am " + name + msg1 + msg2 + msg3);
     }
 
     @RunTimes(times = 2)
